b00016afc07933661f57e046cc6787e66ab95049
Moritz Golombek <moritz.golombek@students.fh-ooe.at>
2024-01-04 16:56:20 +0100

    defined default values for all call arguments

--- S2310787008.py
+++ S2310787008.py
@@ -1,4 +1,4 @@
-__doc__ = "Main method"
+__doc__ = "Main method for the calculations"
 
 #import system libs
 import argparse
@@ -20,23 +20,30 @@ from scipy import constants
 #based on
 #source: https://docs.python.org/3/library/argparse.html
 parser = argparse.ArgumentParser(description="Process some integers.")
-parser.add_argument("--mass", type=int, help="mass of vehicle")
-parser.add_argument("--velocity", type=int, help="initial velocity of vehicle")
-parser.add_argument("--friction", type=int, help="friction coefficient of surface")
-parser.add_argument("--inclination", type=int, help="inclination of surface")
+#listing all cll arguments of the program, consisting of mass, velocity, friction and inclination 
+parser.add_argument("--mass", type=int, default=1000, help="mass of vehicle")
+parser.add_argument("--velocity", type=int, default=50, help="initial velocity of vehicle")
+parser.add_argument("--friction", type=int, default=0.65, choices=(0.1, 0.2, 0.4, 0.65), help="friction coefficient of surface")
+parser.add_argument("--inclination", type=int, default=0, choices=range(0,100), help="inclination of surface")
 args = parser.parse_args()
 
-print (args.mass)
+print ("Your chosen vehicle mass is",args.mass)
+print ("Your chosen vehicle speed is",args.velocity)
+print ("Your chosen friction coefficient is",args.friction)
+print ("Your chosen inclination is",args.inclination)
 
 #===============
-# a method
-def main_method():
-  main_method.__doc__ = "sample main method"
-# exec_sample_plot_(cmd_call_args_.pdf_file_out)
+#brake distance calculation method
+def distance_method():
+  distance_method.__doc__ = "calculation of braking distance"
+  
+#velocity over time under breaking
+def velocity_method():
+  velocity_method.__doc__ = "calculation of braking velocity"
 
 #===============
 # do work and call a methode
-main_method()
+#method()
 
 #terminate program
 sys.exit()
