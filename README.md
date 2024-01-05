cf6a1b12afff1487b1156222305fa533d4b21cf3
Moritz Golombek <moritz.golombek@students.fh-ooe.at>
2024-01-05 03:08:55 +0100

    added all basic functions and formulas required

--- README.md
+++ README.md
@@ -2,10 +2,37 @@ Name: Moritz Golombek
 Student ID: S2310787008
 
 Sample call statement: 
-Python S2310787008.py --mass=1000, --velocity=50, --friction=0.65
+Python S2310787008.py --mass=1000, --velocity=50, --friction=0.65, --inclination=10
 
 Standard values:
 - mass:         1000 [kg]
 - velocity:     50 [km/h]
 - friction:     0.65 [-]
-- inclination:  0 [%]
+- inclination:  0 [deg]
+
+How the problem is solved: 
+
+1.  The call arguments are processed. This is necessary, as the units used for user convenience during the entering process are not all in suitable Si units.
+    Namely the conversion of km/h to m/s is conducted here and a conversion of the incline in degree to radians is conducted. 
+
+2.  The stopping distance is calculated by setting the kinetic energy of the vehicle in motion equal to the Work "conducted" by the friction over a distance.
+    The formulas are Wkin = 1/2*m*v0^2, Wr = my*m*g*d with m = mass, my = friction coefficient, g = gravitational force, d = distance, v0 = initial speed.
+    To incorporate also inclines or declines, which affect the stopping distance, the formula for Wr was extended to Wr = s*m*g*(my*cos(a)+sin(a)) with p = angle of incline.
+    The final simplification of the formula is: s = v0^2/2*g*(my*cos(p)+sin(p)).
+
+3.  With the stopping distance known, the acceleration (in this case deceleration because of the negative sign) can be calculated. 
+    Derived from the 2nd law of motion s = s0 + v0*t + 1/2*a*t^2 with a = acceleration the formula a = (vf^2-v0^2)/2*s is derived with vf = final velocity, in this case 0 m/s.
+
+4.  In relation is then used to calculated the stopping time with t = 2*s/v0. 
+    The value calculated for the stopping time is then used as boundary range for the plot functions in the linspace-definition.
+
+5.  For velocity / time plot, needing a time-dependant velocity function, the relation a = v/t can be used together with the initial velocity v0. 
+    The following formula results: v = a*t+v0.
+
+6.  The distance / time plot again uses the 2nd law of motion with the resulting formula being used: s = 1/2*a*t^2+v0*t.
+
+7.  The last step is setting the correct labels and plot settings so the result is displayed correctly. The matplotlib.org help-site was used to find the correct arguments.
+
+8.  Besides the graphs the user can also get the most useful information from the console. Values like the entered variables, stopping distance, deceleration and stopping time are displayed.
+    The standard "Rule-of-Thumb" formula for the calculation of an emergency braking distance is also implemented and given as an output for comparison to the user.
+    This is done without and with a reaction time, while the calculations above show the distance needed from fully applied brakes to stand-still. 
--- S2310787008.py
+++ S2310787008.py
@@ -3,6 +3,7 @@ __doc__ = "Main method for the calculations"
 #import system libs
 import argparse
 import sys
+import math
 #----
 #copy code
 #source: https://matplotlib.org/stable/users/explain/quick_start.html
@@ -20,30 +21,73 @@ from scipy import constants
 #based on
 #source: https://docs.python.org/3/library/argparse.html
 parser = argparse.ArgumentParser(description="Process some integers.")
-#listing all cll arguments of the program, consisting of mass, velocity, friction and inclination 
-parser.add_argument("--mass", type=int, default=1000, help="mass of vehicle")
-parser.add_argument("--velocity", type=int, default=50, help="initial velocity of vehicle")
+#listing all call arguments of the program, consisting of mass, velocity, friction and inclination 
+parser.add_argument("--mass", type=int, default=1000, help="mass of vehicle in kg")
+parser.add_argument("--velocity", type=int, default=50, help="initial velocity of vehicle in km/h")
 parser.add_argument("--friction", type=int, default=0.65, choices=(0.1, 0.2, 0.4, 0.65), help="friction coefficient of surface")
-parser.add_argument("--inclination", type=int, default=0, choices=range(0,100), help="inclination of surface")
+parser.add_argument("--inclination", type=int, default=0, help="inclination of surface in deg")
 args = parser.parse_args()
 
-print ("Your chosen vehicle mass is",args.mass)
-print ("Your chosen vehicle speed is",args.velocity)
+#display input values to user for confirmation
+print ("Your chosen vehicle mass is",args.mass, "kg")
+print ("Your chosen vehicle speed is",args.velocity, "km/h")
 print ("Your chosen friction coefficient is",args.friction)
-print ("Your chosen inclination is",args.inclination)
+print ("Your chosen inclination is",args.inclination, "deg")
+
+#convert inputs to correct units
+in_speed = args.velocity*constants.kmh
+incline = math.radians(args.inclination)
+
+#calculate stopping distance for braking manoeuvre
+stopping_distance = (in_speed**2)/(2*constants.g*(args.friction*math.cos(incline)+math.sin(incline)))
+print ("The stopping distance is", stopping_distance,"m")
+
+distance_rot = 0.5*(args.velocity/10)**2
+print ("The emergency brake stopping distance according to the RoT would be", distance_rot, "m (including reaction time:", distance_rot+(args.velocity/10)*3, "m)")
+
+#calculate deceleration during braking manoeuvre
+deceleration = (0**2-in_speed**2)/(2*stopping_distance)
+print ("The deceleration is", deceleration/constants.g, "G, or", deceleration, "m/s^2")
+
+#calculate time needed for braking manoeuvre
+stopping_time = 2*stopping_distance/in_speed
+print ("The time needed to stop is", stopping_time, "s")
+
+#setting the length of the x-axis to the duration of the braking manoeuvre
+time = np.linspace(0,stopping_time)
 
 #===============
-#brake distance calculation method
-def distance_method():
-  distance_method.__doc__ = "calculation of braking distance"
-  
-#velocity over time under breaking
-def velocity_method():
-  velocity_method.__doc__ = "calculation of braking velocity"
+#velocity during breaking manoeuvre
+def current_velocity(time):
+  current_velocity.__doc__ = "calculation of the current velocity during braking"
+  return deceleration*time+in_speed
 
 #===============
-# do work and call a methode
-#method()
+#distance traveled during breaking manoeuvre
+def current_distance(time):
+  current_distance.__doc__="calculation of the distance traveled so far under braking"
+  return (deceleration*time**2)/2+in_speed*time
+
+#def distance_rot(time):
+#  distance_rot.__doc__="calculation of the distance required according to Rule of Thumb"
+#  return 
+
+#===============
+#supplying the desired graph plot settings
+#based on
+#source: https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subplots_demo.html
+fig, (ax1,ax2) = plt.subplots(1,2)
+ax1.grid (True)
+ax2.grid (True)
+ax1.plot(time, current_velocity(time))
+ax2.plot(time, current_distance(time), distance_rot)
+ax1.set_xlabel ('time [s]')
+ax2.set_xlabel ('time [s]')
+ax1.set_ylabel ('velocity [m/s]')
+ax2.set_ylabel ('distance [m]')
+ax1.set_title ('Velocity over Time')
+ax2.set_title ('Distance over Time')
+plt.show()
 
 #terminate program
 sys.exit()
